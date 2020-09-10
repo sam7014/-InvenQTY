49f7fd0852a3f291afcdc6a3be8c2f43a7bb768b
LSY <kastenguck@naver.com>
2020-09-10 22:10:16 +0900

    음...

--- app/src/main/java/com/example/invenqty/MainActivity.kt
+++ app/src/main/java/com/example/invenqty/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.invenqty
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,29 +20,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 문자열 배열과 기본 스피너 레이아웃을 사용하여 ArrayAdapter만들기
+        val spinnerAdapter : ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(this,R.array.select_Gubun,android.R.layout.simple_spinner_item)
+        // 선택 목록이 나타날 때 사용할 레이아웃 지정
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // 스피너에 어댑터 적용
+        spinner_gubun.adapter = spinnerAdapter
+        //
+        spinner_gubun.setSelection(0)
+
+
+        val keyword = editText_keyword.text.toString() // 왜 변수에 넣어서 던지면 안되지?
         val adapter = StockAdapter()
         recyclerView.adapter = adapter
-
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        val retrofit = Retrofit.Builder()
-                .baseUrl("https://svc.toocool.co.kr/")
+        button_getQTY.setOnClickListener {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://test.toocool.co.kr/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
-        val api = retrofit.create(tcfsAPI::class.java)
-        val callGetStock = api.getStock("다이노플라츠바치오브라키오8호")
+            val api = retrofit.create(tcfsAPI::class.java)
+            val callGetStock = api.getStock(editText_keyword.text.toString()) // 왜 변수에 넣어서 던지면 안되지? 여기? 쿼리타입이여서?
 
-        callGetStock.enqueue(object : Callback<Res_Stock> {
-            override fun onResponse(call: Call<Res_Stock>, response: Response<Res_Stock>) {
-                Log.d("결과","성공 : ${response.raw()}")
-                adapter.stockList.addAll(response.body()!!.reponse_Data)
-                adapter.notifyDataSetChanged()
-                println(adapter.stockList)
-            }
+            callGetStock.enqueue(object : Callback<Res_Stock> {
+                override fun onResponse(call: Call<Res_Stock>, response: Response<Res_Stock>) {
+                    Log.d("결과","성공 : ${response.raw()}")
+                    adapter.stockList.addAll(response.body()!!.reponse_Data)
+                    adapter.notifyDataSetChanged()
+                    println(adapter.stockList)
+                }
 
-            override fun onFailure(call: Call<Res_Stock>, t: Throwable) {
-                Log.d("결과","실패 : $t")
-            }
-        })
+                override fun onFailure(call: Call<Res_Stock>, t: Throwable) {
+                    Log.d("결과","실패 : $t")
+                }
+            })
+        }
+
     }
 }
--- app/src/main/java/com/example/invenqty/StockInfo.kt
+++ app/src/main/java/com/example/invenqty/StockInfo.kt
@@ -1,13 +1,45 @@
 package com.example.invenqty
 
 import com.google.gson.annotations.SerializedName
+import java.text.DecimalFormat
 
 data class StockInfo (
-    @SerializedName("shopCode") var shopCode : String = "",
-    @SerializedName("shopName") var shopName : String = "",
-    @SerializedName("productCode") var productCode : String = "",
-    @SerializedName("productName") var productName : String = "",
-    @SerializedName("managerCode") var managerCode : String = "",
-    @SerializedName("productPrice") var productPrice : Int = 0,
-    @SerializedName("stockCount") var QTY : Int = 0
+    @SerializedName("shopCode") var shopCode : String,
+    @SerializedName("shopName") var shopName : String,
+    @SerializedName("productCode") var productCode : String,
+    @SerializedName("productName") var productName : String,
+    @SerializedName("managerCode") var managerCode : String,
+    @SerializedName("productPrice") var productPrice : Int,
+    @SerializedName("stockCount") var QTY : Int
 )
+
+class StockInfo2{
+    @SerializedName("shopCode") var shopCode : String = ""
+    @SerializedName("shopName") var shopName : String = ""
+    @SerializedName("productCode") var productCode : String = ""
+    @SerializedName("productName") var productName : String = ""
+    @SerializedName("managerCode") var managerCode : String = ""
+    @SerializedName("productPrice") var productPrice : Int = 0
+    @SerializedName("stockCount") var QTY : Int = 0
+
+    val getManagerCode2Korean : String
+        get() {
+        return when(this.managerCode){
+            "01" -> "운영"
+            "02" -> "단종"
+            "03" -> "단예"
+            "04" -> "해외"
+            else -> this.managerCode
+        }
+    }
+
+    val getProductPriceWithComma : String
+        get() {
+        val formatter = DecimalFormat("###,###")
+        return formatter.format(this.productPrice)
+    }
+
+    val getShopName : String get() {
+        return this.shopName.replace("창고","")
+    }
+}
